c44091027f727140352d1b12b9750ced33b643e0
snus xD <voroninkosta31@gmail.com>
2026-02-08 16:56:44 +0300

    Initial Commit

---
+++ appsmute/appsmuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsmuteApp.swift
+//  appsmute
+//
+//  Created by snus on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct appsmuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
